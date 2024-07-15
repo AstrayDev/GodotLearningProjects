2b75b50b995e4f2ffa8b26054091c8e522a24642
AstrayDev <monotonemackerel@gmail.com>
2024-07-14 22:29:29 -0400

    made semi working borders. rotation is completely busted though

--- Tetris/Assets/Scripts/piece.gd
+++ Tetris/Assets/Scripts/piece.gd
@@ -2,17 +2,20 @@ extends Node
 
 class_name Piece
 
-const i_block = [Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(3, 0)]
-const l_block = [Vector2(0, 0), Vector2(0, 1), Vector2(1, 0), Vector2(2, 0)]
-const j_block = [Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(2, 1)]
-const t_block = [Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(2, 0)]
-const s_block = [Vector2(0, 1), Vector2(1, 1), Vector2(1, 0), Vector2(2, 0)]
-const z_block = [Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(2, 1)]
-const o_block = [Vector2(0, 0), Vector2(0, 1), Vector2(1, 0), Vector2(1, 1)]
+const i_block = [Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0), Vector2i(3, 0)]
+const l_block = [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 0), Vector2i(2, 0)]
+const j_block = [Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0), Vector2i(2, 1)]
+const t_block = [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 0)]
+const s_block = [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 0), Vector2i(2, 0)]
+const z_block = [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1)]
+const o_block = [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 0), Vector2i(1, 1)]
 
 var current_cells = []
 var old_cells = []
-var current_color
+var test_cells = []
+var current_position: Vector2i
+var current_color: Vector2i
+var spawn_position = Vector2i(34, 9)
 
 func set_rand_piece():
 	var rand_num = randi() % 7
@@ -20,25 +23,28 @@ func set_rand_piece():
 	match rand_num:
 		0:
 			current_cells += i_block
-			current_color = Vector2(1, 1)
+			current_color = Vector2i(1, 1)
 		1:
 			current_cells += l_block
-			current_color = Vector2(0, 0)
+			current_color = Vector2i(0, 0)
 		2:
 			current_cells += j_block
-			current_color = Vector2(0, 1)
+			current_color = Vector2i(0, 1)
 		3:
 			current_cells += t_block
-			current_color = Vector2(1, 2)
+			current_color = Vector2i(1, 2)
 		4:
 			current_cells += s_block
-			current_color = Vector2(0, 3)
+			current_color = Vector2i(0, 3)
 		5:
 			current_cells += z_block
-			current_color = Vector2(0, 2)
+			current_color = Vector2i(0, 2)
 		6:
 			current_cells += o_block
-			current_color = Vector2(1, 3)
+			current_color = Vector2i(1, 3)
 
-func _ready():
-	pass
+func rotate_piece():
+	for i in current_cells.size():
+		var new_x = current_cells[i].y * -1
+		current_cells[i].y = current_cells[i].x
+		current_cells[i].x = new_x
--- Tetris/Assets/Scripts/tilemap.gd
+++ Tetris/Assets/Scripts/tilemap.gd
@@ -1,27 +1,66 @@
 extends TileMap
 
 @onready var piece = Piece.new()
+var left_bounds = Vector2i(29, 0)
+var right_bounds = Vector2i(40, 0)
 
 func _ready():
+	piece.set_rand_piece()
 	spawn_piece()
 
 func _process(_delta):
 	if Input.is_action_pressed("move_down"):
-		move(Vector2(0, 1))
+		move(Vector2i.DOWN)
 
 	if Input.is_action_just_pressed("move_right"):
-		move(Vector2(1, 0))
+		move(Vector2i.RIGHT)
 
 	if Input.is_action_just_pressed("move_left"):
-		move(Vector2(-1, 0))
+		move(Vector2i.LEFT)
+
+	if Input.is_action_just_pressed("ui_accept"):
+		piece.rotate_piece()
+
+	print(piece.current_cells)
 
 func spawn_piece():
-	piece.set_rand_piece()
-	piece.old_cells = piece.current_cells
+	var spawn_pos = []
+	var spawn_offset
 
 	for pos in piece.current_cells:
-		set_cell(0, pos, 0, piece.current_color)
+		spawn_offset = pos + piece.spawn_position
+		set_cell(0, spawn_offset, 0, piece.current_color)
+		spawn_pos.append(spawn_offset)
 
-func move(dir: Vector2):
-			for pos in piece.current_cells:
-				set_cell(0, pos + dir, 0, piece.current_color)
+	piece.current_cells = spawn_pos
+
+func move(dir: Vector2i):
+	var new_cells = []
+	piece.old_cells = piece.current_cells
+
+	# deletes old cells
+	for pos in piece.old_cells:
+		erase_cell(0, pos)
+
+	for pos in piece.current_cells:
+		piece.test_cells.append(pos + dir)
+		if is_valid_move():
+			piece.current_position = pos + dir
+			set_cell(0, piece.current_position, 0, piece.current_color)
+			new_cells.append(piece.current_position)
+		else:
+			piece.current_cells = piece.old_cells
+			for i in piece.current_cells:
+				set_cell(0, i, 0, piece.current_color)
+			piece.test_cells = []
+			return
+
+	piece.old_cells = piece.current_cells
+	piece.current_cells = new_cells
+	piece.test_cells = []
+
+func is_valid_move() -> bool:
+	for pos in piece.test_cells:
+		if pos < left_bounds or pos > right_bounds:
+			return false
+	return true
