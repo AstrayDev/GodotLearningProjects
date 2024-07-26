e7a9c2deec5dd166c2f4892f64154c906920da09
AstrayDev <monotonemackerel@gmail.com>
2024-07-26 03:47:41 -0400

    fixed an issue where cells would fill when they shouldnt, rotation and row deletion is added though not push down

--- Tetris/Assets/Scripts/piece.gd
+++ Tetris/Assets/Scripts/piece.gd
@@ -2,16 +2,17 @@ extends Node
 
 class_name Piece
 
-const i_block = [Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0)]
-const l_block = [Vector2i(-1, 0), Vector2i(-1, 1), Vector2i(0, 0), Vector2i(1, 0)]
-const j_block = [Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1)]
-const t_block = [Vector2i(-1, 0), Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 0)]
-const s_block = [Vector2i(-1, 1), Vector2i(0, 1), Vector2i(0, 0), Vector2i(1, 0)]
-const z_block = [Vector2i(-1, 0), Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1)]
+const i_block = [Vector2i( - 1, 0), Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0)]
+const l_block = [Vector2i( - 1, 0), Vector2i( - 1, 1), Vector2i(0, 0), Vector2i(1, 0)]
+const j_block = [Vector2i( - 1, 0), Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1)]
+const t_block = [Vector2i( - 1, 0), Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 0)]
+const s_block = [Vector2i( - 1, 1), Vector2i(0, 1), Vector2i(0, 0), Vector2i(1, 0)]
+const z_block = [Vector2i( - 1, 0), Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1)]
 const o_block = [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 0), Vector2i(1, 1)]
 
 var current_cells = []
 var old_cells = []
+var test_cells = []
 var rotation_cells = []
 var blocktype_cells = []
 var current_position: Vector2i
@@ -48,12 +49,22 @@ func set_rand_piece():
 	current_cells = blocktype_cells
 	rotation_cells = blocktype_cells
 
-func rotate_left():
-	for i in rotation_cells.size():
-		var new_x = rotation_cells[i].y * -1
-		rotation_cells[i].y = rotation_cells[i].x
-		rotation_cells[i].x = new_x
-		
+func rotate_piece(dir: int): # dir is used to decided which direction to rotate and I'm just using 0 and 1 to decide
+	var new_x
+	var new_y
+	var new_rotation_cells = []
 
-	for j in current_cells.size():
-		current_cells[j] += rotation_cells[j]
+	for i in rotation_cells.size():
+		if dir == 0:
+			new_x = rotation_cells[i].y * - 1
+			new_y = rotation_cells[i].x
+			new_rotation_cells.append(Vector2i(new_x, new_y))
+
+		elif dir == 1:
+			new_x = rotation_cells[i].y
+			new_y = rotation_cells[i].x * -1
+			new_rotation_cells.append(Vector2i(new_x, new_y))
+	
+	rotation_cells = new_rotation_cells
+	for i in current_cells.size():
+		current_cells[i] = current_position + rotation_cells[i]
--- Tetris/Assets/Scripts/tilemap.gd
+++ Tetris/Assets/Scripts/tilemap.gd
@@ -18,22 +18,30 @@ func _process(_delta):
 
 	if Input.is_action_just_pressed("turn_left"):
 		piece.old_cells = piece.current_cells
-		for pos in piece.old_cells:
-			erase_cell(0, pos)
+		for i in piece.old_cells:
+			erase_cell(0, i)
 			
-		piece.rotate_left()
+		piece.rotate_piece(1)
 
 		for i in piece.current_cells:
 			set_cell(0, i, 0, piece.current_color)
 
-	print(piece.current_cells)
+	if Input.is_action_just_pressed("turn_right"):
+		piece.old_cells = piece.current_cells
+		for pos in piece.old_cells:
+			erase_cell(0, pos)
+			
+		piece.rotate_piece(0)
+
+		for i in piece.current_cells:
+			set_cell(0, i, 0, piece.current_color)
 
 func spawn_piece():
 	var spawn_pos = []
 	var spawn_offset
 
-	for pos in piece.current_cells:
-		spawn_offset = pos + piece.spawn_position
+	for i in piece.current_cells:
+		spawn_offset = i + piece.spawn_position
 		set_cell(0, spawn_offset, 0, piece.current_color)
 		spawn_pos.append(spawn_offset)
 
@@ -43,42 +51,66 @@ func move(dir: Vector2i):
 	var new_cells = []
 	piece.old_cells = piece.current_cells # assign last known valid position
 
-	for pos in piece.old_cells:
-		erase_cell(0, pos)
-
 	for pos in piece.current_cells:
-		piece.test_position = pos + dir
-		if is_valid_move():
-			if should_stack_piece():
-				return
-			else:
-				piece.current_position = pos + dir
+		piece.test_cells.append(pos + dir)
+
+	for i in piece.old_cells:
+		erase_cell(0, i)
+
+	if is_valid_move():
+		if should_stack_piece():
+			for j in piece.current_cells:
+				set_cell(0, j, 0, piece.current_color)
+			reset_piece()
+		else:
+			for i in piece.test_cells:
+				piece.current_position = i
 				set_cell(0, piece.current_position, 0, piece.current_color)
 				new_cells.append(piece.current_position)
+			piece.current_cells = new_cells
 
-		else: # if move isn't valid return to previous position
-			piece.current_cells = piece.old_cells
-			for old_pos in piece.current_cells:
-				set_cell(0, old_pos, 0, piece.current_color)
-			return
-			
-	piece.current_cells = new_cells
+	else: # if move isn't valid return to previous position
+		for old_pos in piece.old_cells:
+			set_cell(0, old_pos, 0, piece.current_color)
+
+	piece.test_cells.clear() # reset test cells for next move
 
 func is_valid_move() -> bool:
-	var id = get_cell_alternative_tile(0, piece.test_position)
-	if id == 3:
-		return false
+	var id: int
+	for i in piece.test_cells:
+		id = get_cell_alternative_tile(0, i)
+		if id == 3:
+			return false
 	return true
 
 func should_stack_piece() -> bool:
-	var id = get_cell_source_id(0, piece.test_position)
-
-	if id == 0:
-		for pos in piece.current_cells:
-			set_cell(0, pos, 0, piece.current_color)
-		piece.free()
-		piece = Piece.new()
-		piece.set_rand_piece()
-		spawn_piece()
-		return true
+	var id: int
+	for i in piece.test_cells:
+		id = get_cell_source_id(0, i)
+		if id == 0 || id == 5:
+			return true
 	return false
+
+func reset_piece():
+	check_rows()
+	piece.free()
+	piece = Piece.new()
+	piece.set_rand_piece()
+	spawn_piece()
+
+func check_rows():
+	for y in range(9, 31):
+		var full_row = []
+		var row_full = true
+		for x in range(29, 40):
+			full_row.append(Vector2i(x, y))
+		for i in full_row:
+			if get_cell_source_id(0, i) == -1:
+				row_full = false
+				break  # exit the loop early if any cell is empty
+		if row_full:
+			erase_row(full_row)
+            
+func erase_row(row: Array):
+	for x in row:
+		erase_cell(0, x)
